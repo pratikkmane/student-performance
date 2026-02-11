9faa325c224189fe954e3a1c1e5ca8a49fcdabeb
Pratik Mane <manep@iu.edu>
2026-02-11 10:48:37 -0500

    Initial code for app

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Student Performance", layout="wide")
+st.title("Student Academic Performance Predictor")
+st.write("Use the sidebar to navigate between pages.")
